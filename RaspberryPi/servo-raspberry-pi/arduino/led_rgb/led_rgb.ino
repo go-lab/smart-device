1bf5bd8b099aec1d9a4e6ecef2e2150f65d645dc
Wissam-Halimi <wissam.halimi@epfl.ch>
2014-07-04 14:29:19 +0200

    Servo with Raspbeery Pi

---
+++ RaspberryPi/servo-raspberry-pi/arduino/led_rgb/led_rgb.ino
@@ -0,0 +1,73 @@
+
+/***************************************************************/
+/* Author: German Carro Fernandez                                      */
+/* DIEEC, UNED                                                                   */
+/***************************************************************/
+
+#define START_COLOR_CHAR '^'
+#define END_COLOR_CHAR '$'
+#define COLOR_SIZE 8
+#define PIN_RED 9
+#define PIN_GREEN 10
+#define PIN_BLUE 11
+
+char serialMessage[COLOR_SIZE];
+unsigned int readChar;
+unsigned int count;
+unsigned long color;
+unsigned int r;
+unsigned int g;
+unsigned int b;
+boolean readingSerial;
+
+void setup() {
+  Serial.begin(9600);
+  readingSerial = false;
+}
+
+void loop() {
+  if (Serial.available() > 0 && !readingSerial) {
+    if (Serial.read() == START_COLOR_CHAR) {
+      serialReadColor();
+    }
+  }
+}
+
+void serialReadColor() {
+  readingSerial = true;
+  count = 0;
+  
+  iniReading:
+  if (Serial.available() > 0) {
+    readChar = Serial.read();
+    if (readChar == END_COLOR_CHAR || count == COLOR_SIZE) {
+      goto endReading;
+    } else {
+      serialMessage[count++] = readChar;
+      goto iniReading;
+    }
+  }
+  goto iniReading;
+  
+  endReading:
+  readingSerial = false;
+  serialMessage[count] = '\0';
+  
+  setColor(serialMessage);
+}
+
+void setColor(char* value)
+{
+  // Convert Char* to Long
+  color = atol(value);
+  
+  // Extract RGB
+  r = color >> 16 & 0xFF;
+  g = color >>  8 & 0xFF;
+  b = color >>  0 & 0xFF;
+  
+  // Send values to analog pins
+  analogWrite(PIN_RED, r);
+  analogWrite(PIN_GREEN, g);
+  analogWrite(PIN_BLUE, b);
+} 
