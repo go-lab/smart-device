e425ff2d11cf4014d6d39e8762029e1e85bbcfd4
German Carro Fernandez <gmncrf@gmail.com>
2014-07-04 16:48:07 +0200

    Upload the files with new  structure

---
+++ raspberry_pi/robotic_arm-raspberry-pi/modules/serialport/build/Release/.deps/Release/obj.target/serialport/src/serialport_poller.o.d
@@ -0,0 +1,31 @@
+cmd_Release/obj.target/serialport/src/serialport_poller.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src -I/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include -I/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/v8/include -I../node_modules/nan  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/serialport/src/serialport_poller.o.d.raw  -c -o Release/obj.target/serialport/src/serialport_poller.o ../src/serialport_poller.cpp
+Release/obj.target/serialport/src/serialport_poller.o: \
+ ../src/serialport_poller.cpp ../node_modules/nan/nan.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-unix.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv-private/ngx-queue.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-linux.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/v8/include/v8.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/v8/include/v8stdint.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_object_wrap.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_buffer.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_version.h \
+ /var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_object_wrap.h \
+ ../src/serialport_poller.h
+../src/serialport_poller.cpp:
+../node_modules/nan/nan.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-unix.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv-private/ngx-queue.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-linux.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/v8/include/v8.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/deps/v8/include/v8stdint.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_object_wrap.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_buffer.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_version.h:
+/var/www/Raspberry-Pi-Status/node_modules/serialport/.node-gyp/0.10.26/src/node_object_wrap.h:
+../src/serialport_poller.h:
